1dc858838ee8c7812915ff7ebcd8708c8bd02647
kjolenskap <zagudaev2@inbox.ru>
2021-07-23 16:53:04 +0400

    v3.2

--- src/main/java/ru/example/SimbirSoftPractice/repository/RoomDao.java
+++ src/main/java/ru/example/SimbirSoftPractice/repository/RoomDao.java
@@ -1,10 +1,11 @@
 package ru.example.SimbirSoftPractice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.example.SimbirSoftPractice.domain.model.Room;
 
 import java.util.Optional;
-
+@Repository
 public interface RoomDao extends JpaRepository<Room, Long> {
     Optional<Room> findById (Long id);
 }
--- src/main/java/ru/example/SimbirSoftPractice/repository/UserDao.java
+++ src/main/java/ru/example/SimbirSoftPractice/repository/UserDao.java
@@ -1,10 +1,11 @@
 package ru.example.SimbirSoftPractice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.example.SimbirSoftPractice.domain.model.User;
 
 import java.util.Optional;
-
+@Repository
 public interface UserDao extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
 
